4f408b1daa8a16393fd05d22b6417c76d9307295
AndrwPham <anpd2004@gmail.com>
2024-04-25 14:50:52 +0700

    update

---
+++ Day2/BMICal.py
@@ -0,0 +1,8 @@
+#Enter height
+height = input("Enter height (m): ")
+#Enter weight
+weight = input("Enter weight (kg): ")
+h = float(height)
+w = int(weight)
+BMI = w / (h*h)
+print("BMI = ", BMI)
--- Day2/CodEx1.py
+++ Day2/CodEx1.py
@@ -1,8 +1,9 @@
-#add digits in a number
 number = (input("Pick a num: "))
 char = str(number)
-nums = []
 sum = 0
+
+#add digits in a number
+
 for i in char:
     sum = sum + int(i)
 print(sum)
